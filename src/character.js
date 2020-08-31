c42c607dc1ea2ed62c4fbf9e2baafb0d08737de4
JackRandom <andrewglogan1987@gmail.com>
2020-08-31 20:40:44 +0100

    resolved this.level error

--- src/character.js
+++ src/character.js
@@ -4,7 +4,6 @@ function Character(config) {
   this.health = config.health;
   this.maxHealth = config.maxHealth;
   this.dialogue = config.dialogue;
-  this.level = config.level || 1;
 }
 
 Character.prototype = {
